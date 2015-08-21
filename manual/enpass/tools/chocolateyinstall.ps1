421db82ee7fa1ea429dd71e10d82bdcad100e5a9
Sebastian Werner <swerner@outlook.at>
2015-08-21 23:37:50 +0200

    added enpass pkg

---
+++ manual/enpass/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+# REMOVE ANYTHING BELOW THAT IS NOT NEEDED
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName = 'enpass' # arbitrary name for the package, used in messages
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'http://dl.sinew.in/windows/setup/EnpassSetup-4.6.1.exe' # download url
+$url64 = $url # 64bit URL here or remove - if installer is both, use $url
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  # optional
+  registryUninstallerKey = 'enpass' #ensure this is the value in the registry
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ manual/enpass/tools/chocolateyuninstall.ps1
@@ -0,0 +1,39 @@
+﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
+# REMOVE ANYTHING BELOW THAT IS NOT NEEDED
+# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'enpass'
+# registry uninstaller key name is the key that is found at HKLM:\Software\Windows\CurrentVersion\Uninstall\ THE NAME
+$registryUninstallerKeyName = 'enpass' #ensure this is the value in the registry
+$shouldUninstall = $true
+
+$local_key     = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+# local key 6432 probably never exists
+$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName" 
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'EXE' 
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+if (!(Test-Path $file)) {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
