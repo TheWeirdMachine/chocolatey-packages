83b96924b8c02320d04eb03ba2db95a4f9ec7103
Sebastian Werner <swerner@outlook.at>
2017-01-03 23:06:52 +0100

    updated hp-bios-cmdlets to version 1.1.0.1(a)

--- manual/hp-bios-cmdlets/tools/chocolateyinstall.ps1
+++ manual/hp-bios-cmdlets/tools/chocolateyinstall.ps1
@@ -1,34 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$packageName = 'hp-bios-cmdlets' # arbitrary name for the package, used in messages
-$url = 'http://ftp.hp.com/pub/softlib2/software1/pubsw-windows/p2008826436/v118922/HPBIOSCmdlets-x86.exe'
-$url64 = 'http://ftp.hp.com/pub/softlib2/software1/pubsw-windows/p230763598/v118921/HPBIOSCmdlets-x64.exe'
+$packageName = 'hp-bios-cmdlets'
+$url = 'https://downloads.hpe.com/pub/softlib2/software1/pubsw-windows/p2008826436/v125061/HPBIOSCmdlets-x86.exe'
+$url64 = 'https://downloads.hpe.com/pub/softlib2/software1/pubsw-windows/p230763598/v125062/HPBIOSCmdlets-x64.exe'
+$checksum = 'BF8961521330F0157406756ACEF0160FC47245F7DA29BCDEB194E12F56445A86'
+$checksum64 = '3F52A381E4851BEC6BC2FAA619D15B695A2C59A5C62B3C75F1F37FBA17C0BC9C'
+$checksumType = 'sha256'
+$checksumType64 = 'sha256'
 $fileType = 'msi'
 $silentArgs = '/quiet'
-$filePath = "$env:TEMP\chocolatey\$packageName"
-$fileFullPath = "$filePath\$packageName_Install.exe"
+$dlPkg = "$packageName" + ".exe"
+$ValidExitCodes = '0'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-try {
-    if (-not (Test-Path $filePath)) {
-        New-Item -ItemType directory -Path $filePath
-    }
-
-    Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
-	Add-Type -assembly "system.io.compression.filesystem"
-	[io.compression.zipfile]::ExtractToDirectory($fileFullPath, $filePath)
-
-    if (Get-ProcessorBits 64) {
-	    $file = "$filePath\HPBIOSCmdlets-x64.msi"
-	} else {
-	    $file = "$filePath\HPBIOSCmdlets-x86.msi"
-	}
-    
-    Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $file	
-	
-} catch {
-    throw
+if (Get-ProcessorBits 64) {
+    $file = "HPBIOSCmdlets-x64.msi"
+} else {
+    $file = "HPBIOSCmdlets-x86.msi"
 }
 
-if (Test-Path $filePath) {
-    Remove-Item $filePath\* -recurse -force -exclude .exe
-}
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$toolsDir\$dlPkg" -Url $url -Url64 $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
+Get-ChocolateyUnzip -FileFullPath "$toolsDir\$dlPkg" -Destination $toolsDir 
+Install-ChocolateyInstallPackage $packageName $fileType $silentArgs "$toolsDir\$file" -ValidExitCodes $ValidExitCodes
